068b22917b69af6a0592e080e94c71bf64a42e20
Gabriel Louredo <gabriel.louredo@pmm.am.gov.br>
2021-12-25 01:14:28 -0400

    Projeto inicial

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Cod3r</strong> 2021-2022</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+  .footer {
+
+    grid-area: footer;
+    background-color: #ddd;
+    color: #333;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 10px;
+
+ 
+  }
+</style>
---
+++ src/components/template/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="header">
+    <a class="toogle" @click.prevent="toogleMenu" v-if="!hideToogle">
+      <i class="fa fa-lg" :class="icon"></i> 
+    </a>
+    <h1 class="title">{{title}}</h1>
+  </header>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    hideToogle: Boolean 
+  },
+  computed: {
+    ...mapState(['isMenuVisible']),
+    icon(){
+      return this.isMenuVisible ? "fa-angle-left" : "fa-angle-down" 
+    },
+  },
+  methods: {
+    toogleMenu(){
+      this.$store.commit('toogleMenu')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header{
+    grid-area: header;
+    background: linear-gradient(to right, #1e469A, #49a7c1);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  }
+
+  .title {
+    font-size: 1.2rem;
+    color: #fff;
+    font-weight: 100;
+    flex-grow: 1;
+    text-align: center;
+  }
+
+  .header a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  header.header > a.toogle{
+    width: 60px;
+    height: 100%;
+    text-decoration: none;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  header.header > a.toogle:hover{
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.2);
+
+  }
+
+
+</style>
