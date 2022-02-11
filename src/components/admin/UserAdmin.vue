148f9ef7769466b662f2ff98002ef7beeb6daf00
Gabriel Louredo <gabriel.louredo@pmm.am.gov.br>
2022-02-10 20:13:34 -0400

    Criacao da tela de usuarios

--- src/components/admin/UserAdmin.vue
+++ src/components/admin/UserAdmin.vue
@@ -1,8 +1,42 @@
 <template>
   <div class="user-admin">
-    <b-table hover striped :items="users" :fields="fields">
-
-    </b-table>
+    <b-form>
+      <input id="user-id" type="hidden" v-model="user.id"/>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Nome:" label-for="user-name">
+            <b-form-input id="user-name" type="text" v-model="user.name" required placeholder="Informe o nome do usuário"/>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="E-mail:" label-for="user-email">
+            <b-form-input id="user-email" type="text" v-model="user.email" required placeholder="Informe o e-mail do usuário"/>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+        Administrador?
+      </b-form-checkbox>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Senha:" label-for="user-password">
+            <b-form-input id="user-password" type="password" v-model="user.password" required placeholder="Informe a senha do usuário"/>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="Confirmar Senha:" label-for="user-confirm-password">
+            <b-form-input id="user-confirm-password" type="password" v-model="user.confirmPassword" required placeholder="Confirme a senha do usuário"/>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-button variant="primary" v-if="mode === 'save'"
+        @click="save">Salvar</b-button>
+      <b-button variant="danger" v-if="mode === 'remove'"
+        @click="remove">Excluir</b-button>
+      <b-button class="ml-2" @click="reset">Cancelar</b-button>
+    </b-form>
+    <br/>
+    <b-table hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
 
